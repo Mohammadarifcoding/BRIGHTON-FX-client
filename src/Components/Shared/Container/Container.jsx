e99179999a822eac0cb586e87806e6ff881bd6a4
Mohammadarifcoding <minaraarifa@gmail.com>
2023-12-26 16:20:11 +0600

    Solved the size

--- src/Components/Shared/Container/Container.jsx
+++ src/Components/Shared/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className='max-w-[1300px] mx-auto container  lg:px-10 sm:px-6 px-3'>
+        <div className='max-w-[1470px] mx-auto container  lg:px-10 sm:px-6 px-3'>
             {children}
         </div>
     );
