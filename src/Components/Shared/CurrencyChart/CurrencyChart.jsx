65d2ecb923980648e30158c4856a44fcaa0cea13
Mohammadarifcoding <minaraarifa@gmail.com>
2023-12-31 10:11:05 +0600

    Added the chartItem in the homepage

--- src/Components/Shared/CurrencyChart/CurrencyChart.jsx
+++ src/Components/Shared/CurrencyChart/CurrencyChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../Container/Container';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const CurrencyChart = () => {
     return (
@@ -11,6 +12,35 @@ const CurrencyChart = () => {
                <div className='max-w-[400px] w-full md:mt-0 mt-4'>
                 <input placeholder='Enter currency name' type="text" className='max-w-[400px] min-w-[300px] w-full px-3 py-3 focus:outline-none' />
                </div>
+            
+             </div>
+             <div className='flex flex-col'>
+                   <div className='bg-gray-100 justify-between items-center px-3 flex  py-5'>
+                    <div className='flex gap-5 items-center'>
+                    <h2 className="px-3 bg-gray-200 inline py-3 rounded-lg">
+                         USD
+                     </h2>
+
+                     <h2 className='text-xl'>Euro-EUR</h2>
+                    </div>
+                    
+
+                    <div className='flex  gap-20'>
+                     <div className='flex items-center gap-4'>
+                          <h2 className='text-xl'>1.123</h2>  
+                          <button className='bg-[#93C94E] hover:bg-[#6c923a] hover:text-white text-xl px-3 py-2 rounded-lg flex items-center gap-2'>CLICK & COLLECT <IoIosArrowForward></IoIosArrowForward>  </button>
+                     </div>
+                     <div className='flex items-center gap-4'>
+                          <h2 className='text-xl'>1.123</h2>  
+                          <button className='bg-[#93C94E] hover:bg-[#6c923a] hover:text-white text-xl px-3 py-2 rounded-lg flex items-center gap-2'>CLICK & SELL <IoIosArrowForward></IoIosArrowForward>  </button>
+                     </div>
+
+
+                    </div>
+
+
+                     
+                   </div>
              </div>
             </div>
         </Container>
