f5f31ff3816a9c0ee91da934cf2078a793b8f524
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-26 19:36:13 +0600

    Changed the order of the route

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -10,22 +10,21 @@ import { RxCross1 } from 'react-icons/rx';
 import { AuthContext } from '../../../Provider/AuthProvider';
 import UseAdminCheck from '../../../Hook/UseAdminCheck';
 
-
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const {user , OUT} = useContext(AuthContext)
-    const [checkingAdminAcess,loadingAdminAcess] = UseAdminCheck()
+    const { user, OUT } = useContext(AuthContext);
+    const [checkingAdminAcess, loadingAdminAcess] = UseAdminCheck();
     const toggleDrawer = () => {
         setIsOpen((prevState) => !prevState);
     };
 
-   const handleLogout = ()=>{
-     OUT()
-     .then(res =>{
-      return toast('Log out successfully')
-     })
-     .catch()    
-   }
+    const handleLogout = () => {
+        OUT()
+            .then((res) => {
+                return toast('Log out successfully');
+            })
+            .catch();
+    };
 
     return (
         <div className=" bg-[#93C94E] relative">
@@ -38,14 +37,14 @@ const Navbar = () => {
                     </Link>
 
                     <div className="lg:flex hidden gap-[12px] list-none lg:text-sm md:text-base xl:text-base text-base  bigScreen-navitem items-center">
-                        <NavLink to={'/sendmony'} className=" pb-2">
-                            <li>Send Money </li>
+                        <NavLink to={'/travleMoney'} className=" pb-2">
+                            <li>Travel Money </li>
                         </NavLink>
                         <NavLink to={'/exchangeRates'} className=" pb-2">
                             <li>Exchange Rates </li>
                         </NavLink>
-                        <NavLink to={'/travleMoney'} className=" pb-2">
-                            <li>Travel Money </li>
+                        <NavLink to={'https://online.worthingfx.com/'} className=" pb-2">
+                            <li>Money Transfers </li>
                         </NavLink>
                         <NavLink to={'/termsCoditition'} className=" pb-2">
                             <li>Terms & Condition </li>
@@ -53,36 +52,35 @@ const Navbar = () => {
                         <NavLink to={'/contact'} className=" pb-2">
                             <li>Contact Us </li>
                         </NavLink>
-                        {
-                           user ?  <div className="dropdown dropdown-end">
-                           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                               <div className="w-10 rounded-full">
-                                   <img alt="Tailwind CSS Navbar component" src={user?.photoURL || '/Images/user.png'} />
-                               </div>
-                           </div>
-                           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                               <li>
-                                   <a className="justify-between">
-                                     {user?.email}
+                        {user ? (
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img alt="Tailwind CSS Navbar component" src={user?.photoURL || '/Images/user.png'} />
+                                    </div>
+                                </div>
+                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li>
+                                        <a className="justify-between">{user?.email}</a>
+                                    </li>
+                                    {checkingAdminAcess ? (
+                                        <li>
+                                            <Link to={'/dashboard/Currency'}>Dashboard</Link>
+                                        </li>
+                                    ) : (
+                                        ''
+                                    )}
 
-                                   </a>
-                               </li>
-                               {
-                                checkingAdminAcess? <li>
-                                <Link to={'/dashboard/Currency'}>Dashboard</Link>
-                            </li> : ''
-                               }
-                               
-                               <li>
-                                   <a onClick={handleLogout}>Logout</a>
-                               </li>
-                           </ul>
-                       </div> : <Link to={'/signin'}>
-                            <button className="bg-[#4A54A4] hover:bg-[#32396d] px-5 py-3 rounded-xl text-white">Sign In</button>
-                        </Link>
-                        }
-                        
-                       
+                                    <li>
+                                        <a onClick={handleLogout}>Logout</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        ) : (
+                            <Link to={'/signin'}>
+                                <button className="bg-[#4A54A4] hover:bg-[#32396d] px-5 py-3 rounded-xl text-white">Sign In</button>
+                            </Link>
+                        )}
                     </div>
 
                     <div className="lg:hidden block">
@@ -99,14 +97,14 @@ const Navbar = () => {
                             </div>
 
                             <div className="flex flex-col list-none gap-4 mt-4 text-white">
-                                <NavLink to={'/sendmony'} className=" pb-2 px-4 border-b border-white">
-                                    <li>Send Money </li>
+                                <NavLink to={'/travleMoney'} className=" pb-2 px-4 border-b border-white">
+                                    <li>Travel Money </li>
                                 </NavLink>
                                 <NavLink to={'/exchangeRates'} className=" pb-2 px-4 border-b border-white">
                                     <li>Exchange Rates </li>
                                 </NavLink>
-                                <NavLink to={'/travleMoney'} className=" pb-2 px-4 border-b border-white">
-                                    <li>Travel Money </li>
+                                <NavLink to={'https://online.worthingfx.com/'} className=" pb-2 px-4 border-b border-white">
+                                    <li>Money Transfers </li>
                                 </NavLink>
                                 <NavLink to={'/termsCoditition'} className=" pb-2 px-4 border-b border-white">
                                     <li>Terms & Condition </li>
@@ -114,34 +112,34 @@ const Navbar = () => {
                                 <NavLink to={'/contact'} className=" pb-2 px-4 border-b border-white">
                                     <li>Contact Us </li>
                                 </NavLink>
-                                {
-                           user ?  <div className="dropdown text-black dropdown-end">
-                           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                               <div className="w-10 rounded-full">
-                                   <img alt="Tailwind CSS Navbar component" src={user?.photoURL || '/Images/user.png'} />
-                               </div>
-                           </div>
-                           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                               <li>
-                                   <a className="justify-between">
-                                     {user?.email}
-
-                                   </a>
-                               </li>
-                               {
-                                checkingAdminAcess? <li>
-                                <Link to={'/dashboard/Currency'}>Dashboard</Link>
-                            </li> : ''
-                               }
-                               <li>
-                                   <a onClick={handleLogout}>Logout</a>
-                               </li>
-                           </ul>
-                       </div> : <Link to={'/signin'} className="block border-b border-white">
-                                    <button className="bg-[#4A54A4] hover:bg-[#32396d]  px-4 pb-2  text-white">Sign In</button>
-                                </Link>
-                        }
-                                
+                                {user ? (
+                                    <div className="dropdown text-black dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img alt="Tailwind CSS Navbar component" src={user?.photoURL || '/Images/user.png'} />
+                                            </div>
+                                        </div>
+                                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                            <li>
+                                                <a className="justify-between">{user?.email}</a>
+                                            </li>
+                                            {checkingAdminAcess ? (
+                                                <li>
+                                                    <Link to={'/dashboard/Currency'}>Dashboard</Link>
+                                                </li>
+                                            ) : (
+                                                ''
+                                            )}
+                                            <li>
+                                                <a onClick={handleLogout}>Logout</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                ) : (
+                                    <Link to={'/signin'} className="block border-b border-white">
+                                        <button className="bg-[#4A54A4] hover:bg-[#32396d]  px-4 pb-2  text-white">Sign In</button>
+                                    </Link>
+                                )}
                             </div>
                         </div>
                     </Drawer>
